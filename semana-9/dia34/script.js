d0fc1fe3e8a320a0eafbe11a670e66835284a529
jaimeneco <jaime.nebot.colom@alumnos.cei.es>
2024-12-11 15:04:32 +0100

    Up2

--- semana-9/dia34/script.js
+++ semana-9/dia34/script.js
@@ -3,17 +3,9 @@
 //---------------------------------------------------
 
 
-
-
 //---------------------------------------------------
 // 1. Constantes y variables
 //---------------------------------------------------
-const formu = document.getElementById("formTarea");
-const txtTarea = document.getElementById("txtTarea");
-const boxTareas = document.getElementById("listaTareas");
-
-const listaDeTareas = ["Ordenar el código JS", "Crear las Funciones", "Probar el programa"];
-
 
 
 //---------------------------------------------------
@@ -22,45 +14,16 @@ const listaDeTareas = ["Ordenar el código JS", "Crear las Funciones", "Probar e
 //---------------------------------------------------
 
 
-
-
 //---------------------------------------------------
 // 3. Funciones principales de nuestra web/programa/app.
 //---------------------------------------------------
-function mostrarTareas() {
-
-    // Borrar todo el contenido de la caja
-    boxTareas.innerHTML = "";
-
-    // Cargamos las nuevas tareas
-    listaDeTareas.forEach( (tarea) => {
-        console.log("Tarea es: ", tarea);
-        boxTareas.innerHTML += `<li>${tarea}</li>`;
-    });
-}
 
 
 //---------------------------------------------------
 // 4. EventListeners
 //---------------------------------------------------
 
-// escuchamos al evento cuando se envía
-formu.addEventListener("submit", (event) => {
-
-    //NO envíes el formulario, porque quiero hacer cosas antes.
-    event.preventDefault();
-
-    const nuevaTarea = txtTarea.value; // el texto del input
-    // alert(nuevaTarea);
-
-    listaDeTareas.push(nuevaTarea); // push() es para agregar nuevo item a la lista.
-
-    mostrarTareas();
-
-});
-
 
 //---------------------------------------------------
 // 5. Inicializar nuestro programa
 //---------------------------------------------------
-mostrarTareas();
